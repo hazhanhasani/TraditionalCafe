05cde2a229963c9474a103e49c5b7c9b4e7ea0e3
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-20 21:00:53 +0330

    Configure Iran timezone and Jalali calendar

---
+++ migrations/0004_locale_time.sql
@@ -0,0 +1,8 @@
+INSERT OR REPLACE INTO app_settings (key, value, updated_at)
+VALUES ('timezone', 'Asia/Tehran', CURRENT_TIMESTAMP);
+
+INSERT OR REPLACE INTO app_settings (key, value, updated_at)
+VALUES ('calendar', 'jalali', CURRENT_TIMESTAMP);
+
+INSERT OR REPLACE INTO app_settings (key, value, updated_at)
+VALUES ('currency', 'toman', CURRENT_TIMESTAMP);
