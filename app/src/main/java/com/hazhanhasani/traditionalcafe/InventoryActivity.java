b18e14eba1b468d1cac4aaa6c8a95efdb20c3c3e
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-09-21 11:53:47 +0330

    Use persistent labels in forms

--- app/src/main/java/com/hazhanhasani/traditionalcafe/InventoryActivity.java
+++ app/src/main/java/com/hazhanhasani/traditionalcafe/InventoryActivity.java
@@ -818,7 +818,7 @@ public class InventoryActivity extends Activity {
     }
 
     private EditText field(String hint, boolean number) {
-        EditText input = new EditText(this);
+        EditText input = new LabeledEditText(this);
         input.setHint(hint);
         input.setTextSize(14);
         input.setTextColor(ink);
